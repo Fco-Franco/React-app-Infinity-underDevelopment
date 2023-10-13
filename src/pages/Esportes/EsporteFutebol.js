436049780fe8e53149b342216a90de876b2dc970
fco franco <125459128+Fco-Franco@users.noreply.github.com>
2023-10-13 08:40:38 -0300

    Add files via upload

--- src/pages/Esportes/EsporteFutebol.js
+++ src/pages/Esportes/EsporteFutebol.js
@@ -8,11 +8,35 @@ function EsporteFutebol() {
     const voltar = useNavigate();
     return (
         <>
-            <div class="FutebolContainer">
-                <h2>Pagina Inicial Futebol</h2>
-                <h3>Página Futebol</h3>
-                <h3>Bem vindo</h3>
-            </div>
+            <h2>Pagina Inicial Futebol</h2>
+            <ul>
+                <div class="futebolContainer">
+                    <li>
+                        <div class="card">
+                            <a class="cardItem" href="https://www.espn.com.br/futebol/time/_/id/9969/bra.cear" target="_blank">
+                                <img class="cardItem" src="https://upload.wikimedia.org/wikipedia/de/e/e9/Ceara_Sporting_Clube_de_Fortaleza.svg" alt="Logo Ceará Sporting Clube">
+                                </img>
+                                Ceará Sporting</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card">
+                            <a class="cardItem" href="https://www.espn.com.br/futebol/" target="_blank">
+                                <img class="cardItem" src="https://upload.wikimedia.org/wikipedia/commons/8/8b/Espn.png" alt="Logo ESPN">
+                                </img>
+                                ESPN</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="card">
+                            <a class="cardItem" href="https://www.cbf.com.br/futebol-brasileiro" target="_blank">
+                                <img class="cardItem" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwR-oFU1_MhlCLwIiZBfHa5UYuyq9fdjt6tErXw3OzyHwkxE0qKtunKBJhZZjjThwRU-k&usqp=CAU" alt="Logo CBF">
+                                </img>
+                                CBF</a>
+                        </div>
+                    </li>
+                </div>
+            </ul>
             <div class="formContatoBTN">
                 <Link class="menuPrincipal_Item" to={"/esporte"}>Inicial Esportes</Link>
                 <br />
